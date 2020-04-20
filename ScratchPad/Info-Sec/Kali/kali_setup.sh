dea30f58b9e79db303aca1ff60569c7be4b11ce2
kali <evincen@yahoo.ca>
2020-04-20 06:49:17 -0400

    Initial Kali setup file

---
+++ ScratchPad/Info-Sec/Kali/kali_setup.sh
@@ -0,0 +1,8 @@
+# Initial Kali setup #
+# Erik Vincent
+# Date: 20-Apr-2020
+# Version 0.1
+
+# Install Dropbox
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+~/.dropbox-dist/dropboxd
